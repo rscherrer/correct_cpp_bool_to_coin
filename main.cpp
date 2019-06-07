9f23e943dd0518f44aebf2d784d39271346dd291
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-07 19:20:03 +0200

    Correct C++ update

--- main.cpp
+++ main.cpp
@@ -1,20 +1,17 @@
-#include <cassert>
 #include <iostream>
 #include <string>
-#include <vector>
 
-/// Implementation of bool_to_coin main function
-int do_main(const std::vector<std::string>& args) 
+int main(int argc, char* argv[]) 
 {
-  if (args.size() != 2) 
+  if (argc != 2) 
   {
     return 1;
   }
-  if (args[1] == "true") 
+  if (std::string(argv[1]) == "true") 
   { 
     std::cout << "heads\n";   
   }
-  else if (args[1] == "false") 
+  else if (std::string(argv[1]) == "false") 
   { 
     std::cout << "tails\n"; 
   }
@@ -22,17 +19,4 @@ int do_main(const std::vector<std::string>& args)
   {
     return 1;
   }
-  return 0;
-}
-
-
-/// bool_to_coin main function, that also tests its implementation
-int main(int argc, char* argv[])
-{
-  assert(do_main( { "bool_to_coin" } ) == 1);
-  assert(do_main( { "bool_to_coin", "true" } ) == 0);
-  assert(do_main( { "bool_to_coin", "false" } ) == 0);
-  assert(do_main( { "bool_to_coin", "nonsense" } ) == 1);
-  assert(do_main( { "bool_to_coin", "true", "false" } ) == 1);
-  return do_main(std::vector<std::string>(argv, argv + argc));
 }
