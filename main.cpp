0009ef998e9f3e6e2796c7c0f12aefb16005fd00
rscherrer <raphael.scherrer@evobio.eu>
2019-06-13 15:21:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,48 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* argv[]) 
-{
-  if (argc != 2) 
-  {
-    return 1;
-  }
-  if (std::string(argv[1]) == "true") 
-  { 
-    std::cout << "heads\n";   
-  }
-  else if (std::string(argv[1]) == "false") 
-  { 
-    std::cout << "tails\n"; 
-  }
-  else 
-  {
-    return 1;
-  }
+/// Main function
+int main(int argc, char * argv[]) {
+
+    try {
+
+        // Initialize the coin side
+        std::string coin = "heads";
+
+        // Check for number of arguments
+        if (argc == 1) {
+            throw std::runtime_error("No argument was provided. Please provide an argument.");
+        }
+
+        if (argc > 2) {
+            throw std::runtime_error("More than one argument were provided. Please provide exactly one argument.");
+        }
+
+        // Convert to string
+        std::string argument(argv[1]);
+
+        // Change coin side if needed and check for validity of the input string
+        if (argument == "false") {
+
+            coin = "tails";
+
+        } else if (argument != "true") {
+
+            throw std::runtime_error("The argument was neither true or false. Please provide either true or false.");
+
+        }
+
+        // Print the result
+        std::cout << coin << '\n';
+
+    }
+    catch(const std::exception &err) {
+
+        std::cerr << "Exception: " << err.what() << '\n';
+
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
+
 }
