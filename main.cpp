2a7aaef309cef0714913320dadfb73db0cbdeb08
rscherrer <raphael.scherrer@evobio.eu>
2019-06-17 11:22:36 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,67 +1,40 @@
-#include <iostream>
+#include <cassert>
 #include <string>
 #include <vector>
-#include <cassert>
+#include <iostream>
 
-/// Do the main
-int doMain(const std::vector<std::string>& args) noexcept
+// Write a program that succeeds if there is just one argument, and fails otherwise
+
+int doMain(const std::vector<std::string>& args)
 {
-    try {
-
-        const size_t argc = args.size();
-
-        // Initialize the coin side
-        std::string coin = "heads";
-
-        // Check for number of arguments
-        if (argc == 1) {
-            throw std::runtime_error("No argument was provided. Please provide an argument.");
-        }
-
-        if (argc > 2) {
-            throw std::runtime_error("More than one argument were provided. Please provide exactly one argument.");
-        }
-
-        // Change coin side if needed and check for validity of the input string
-        if (args[1] == "false") {
-
-            coin = "tails";
-
-        }
-        else if (args[1] != "true") {
-
-            throw std::runtime_error("The argument was neither true or false. Please provide either true or false.");
-
-        }
-
-        // Print the result
-        std::cout << coin << '\n';
-
+    const size_t argc = args.size();
+    if (argc == 2u) {
+        return 0;
     }
-    catch(const std::exception &err) {
+    else {
 
-        std::cerr << "Exception: " << err.what() << '\n';
+        if (argc < 2u) {
+            std::cout << "No argument was provided.\n";
+        }
+        else if (argc > 2u) {
+            std::cout << "More than one argument were provided.\n";
+        }
 
-        exit(EXIT_FAILURE);
+        return 1;
     }
-
-    exit(EXIT_SUCCESS);
 }
 
-/// Main function
 int main(int argc, char * argv[])
 {
 
-    // Tests
-    assert(doMain( { "bool_to_coin" } ) == 1);
-    assert(doMain( { "bool_to_coin", "true" } ) == 0);
-    assert(doMain( { "bool_to_coin", "false" } ) == 0);
-    assert(doMain( { "bool_to_coin", "true", "true" } ) == 1);
-    assert(doMain( { "bool_to_coin", "nonsense" } ) == 0);
-
     const std::vector<std::string> args(argv, argv + argc);
-    const int out = doMain(args);
 
-    return out;
+    const std::string programName(args[0]);
+
+    assert(doMain( { programName } ) == 1);
+    assert(doMain( { programName, "arg1" } ) == 0);
+    assert(doMain( { programName, "arg1", "arg2" } ) == 1);
+
+    return doMain(args);
 
 }
